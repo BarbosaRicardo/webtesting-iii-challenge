4739611df6b9cb56089d5bd3fd423ca74e77ac64
Ricardo <ricardo@Ricardos-Mac-mini.local>
2019-12-17 03:40:36 -0800

    test if can not be closed or opened if locked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react.js';
+import { render, fireEvent} from '@testing-library/react';
+import Controls from '../controls/Controls.js';
+
+test("cannot be closed or opened if locked", () => {
+    const toggle = jest.fn();
+    const { getText } = render (
+        <Controls locked={true} closed={true} toggle={ toggle } />
+    ); //end of render 
+    const openGate = getByText(/open gate/i);
+    fireEvent.click(openGate);
+    expect(toggle).not.toHaveBeenCalled();
+});
+
